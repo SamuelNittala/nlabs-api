921c1937efa2d104314756c89652f4959fd7fc96
SamuelNittala <nittalasamuel7@gmail.com>
2021-10-24 04:44:09 +0530

    final fixes to deployment and added heroku-postman collection

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy,sqlalchemy
 
 db = SQLAlchemy()
 
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
-    db.init_app(app)
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
+db.init_app(app)
 
-    with app.app_context():
-        from . import routes
+with app.app_context():
+    from . import routes
+    try:
         db.create_all()
-        return app
+    except sqlalchemy.exc.OperationalError:
+        None
